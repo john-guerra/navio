4942b3a5077baa43985cd7c20c49caa879effea8
Juan Guillermo Murillo Castillo <jg.murillo10@uniandes.edu.co>
2018-02-26 10:17:49 -0500

    data-explorer added

---
+++ data-explorer/routes/users.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+	// Comment out this line:
+  //res.send('respond with a resource');
+
+  // And insert something like this instead:
+  res.json([{
+  	id: 1,
+  	username: "samsepi0l"
+  }, {
+  	id: 2,
+  	username: "D0loresH4ze"
+  }]);
+});
+
+module.exports = router;
